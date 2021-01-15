3f1667dcf6edfd42bc5342948c0ca0936c5fc3be
Laura Parker <lauramparker78@gmail.com>
2021-01-15 10:48:52 -0500

    Creating db and required tables in schema.sql

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE database employees_db;
+
+USE employees_db;
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    PRIMARY KEY(id),
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8,2) NOT NULL,
+    deapartment_id INT NOT NULL
+);
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    PRIMARY KEY(id),
+    name VARCHAR(30)
+);
+
+  
