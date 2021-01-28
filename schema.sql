08a11de647e687c99a10ecd22924fa0c50cf995b
Laura Parker <lauramparker78@gmail.com>
2021-01-28 13:45:15 -0500

    Updating schema and seed file with foreign key constaints

--- schema.sql
+++ schema.sql
@@ -3,37 +3,43 @@ CREATE database employees_db;
 
 USE employees_db;
 
-CREATE TABLE employee (
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    fullname VARCHAR(61) GENERATED ALWAYS AS (CONCAT(first_name,' ',last_name)) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    manager_name VARCHAR(61),
-    id INT auto_increment NOT NULL,
-    PRIMARY KEY(id)
-
+CREATE TABLE department(
+    id INT NOT NULL,
+    PRIMARY KEY(id),
+    name VARCHAR(30)
 );
 
-
-CREATE TABLE role (
+CREATE TABLE role(
     id INT NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY(id),
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(8,2) NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY (id) REFERENCES employee (role_id),
+    CONSTRAINT fk_department
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE department (
-    id INT NOT NULL,
-    PRIMARY KEY (id),
-    name VARCHAR(30),
-    FOREIGN KEY (id) REFERENCES role (department_id),
- 
+CREATE TABLE employee(
+    id INT auto_increment NOT NULL,
+    PRIMARY KEY(id),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    fullname VARCHAR(61) GENERATED ALWAYS AS (CONCAT (first_name,' ',last_name)) NOT NULL,
+    manager_id INT,
+    manager_name VARCHAR(61),
+    role_id INT NOT NULL,
+    CONSTRAINT fk_role
+    FOREIGN KEY(role_id) REFERENCES role(id) 
 );
 
-SELECT * FROM employee;
-SELECT * FROM role;
-SELECT * FROM department;
-  
+
+
+
+
+
+
+
+
+
+
+
--- seed.sql
+++ seed.sql
@@ -1,3 +1,42 @@
+INSERT INTO department (id, name)
+VALUES (100, 'Sales');
+
+INSERT INTO department (id, name)
+VALUES (200, 'Finance');
+
+INSERT INTO department (id, name)
+VALUES (300, 'IT');
+
+INSERT INTO role (title, salary, id, department_id) 
+VALUES ('Sales Support', 40000, 101, 100);
+
+INSERT INTO role (title, salary, id, department_id) 
+VALUES ('Account Manager', 60000, 102, 100);
+
+INSERT INTO role (title, salary, id, department_id) 
+VALUES ('Sales Director', 80000, 103, 100);
+
+INSERT INTO role (title, salary, id, department_id) 
+VALUES ('Processor', 40000, 201, 200);
+
+INSERT INTO role (title, salary, id, department_id)
+VALUES ('Contracts Specialist', 80000, 202, 200);
+
+INSERT INTO role (title, salary, id, department_id)
+VALUES ('Finance Director', 120000, 203, 200);
+
+INSERT INTO role (title, salary, id, department_id) 
+VALUES ('Sys Admin', 70000, 301, 300);
+
+INSERT INTO role (title, salary, id, department_id)
+VALUES ('Developer', 90000, 302, 300);
+
+INSERT INTO role (title, salary, id, department_id) 
+VALUES ('DBA', 100000, 303, 300);
+
+INSERT INTO role (title, salary, id, department_id) 
+VALUES ('IT Director', 120000, 304, 300);
+
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
 VALUES ('Alex', 'Anderson', 101, 11);
 
@@ -34,41 +73,5 @@ VALUES ('Kenzie', 'King', 303, 31);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
 VALUES ('Lonny', 'Littleton', 304, 35);
 
-INSERT INTO role (title, salary, id, department_id) 
-VALUES ('Sales Support', 40000, 101, 100);
 
-INSERT INTO role (title, salary, id, department_id) 
-VALUES ('Account Manager', 60000, 102, 100);
 
-INSERT INTO role (title, salary, id, department_id) 
-VALUES ('Sales Director', 80000, 103, 100);
-
-INSERT INTO role (title, salary, id, department_id) 
-VALUES ('Processor', 40000, 201, 200);
-
-INSERT INTO role (title, salary, id, department_id)
-VALUES ('Contracts Specialist', 80000, 202, 200);
-
-INSERT INTO role (title, salary, id, department_id)
-VALUES ('Finance Director', 120000, 203, 200);
-
-INSERT INTO role (title, salary, id, department_id) 
-VALUES ('Sys Admin', 70000, 301, 300);
-
-INSERT INTO role (title, salary, id, department_id)
-VALUES ('Developer', 90000, 302, 300);
-
-INSERT INTO role (title, salary, id, department_id) 
-VALUES ('DBA', 100000, 303, 300);
-
-INSERT INTO role (title, salary, id, department_id) 
-VALUES ('IT Director', 120000, 304, 300);
-
-INSERT INTO department (id, name)
-VALUES (100, 'Sales');
-
-INSERT INTO department (id, name)
-VALUES (200, 'Finance');
-
-INSERT INTO department (id, name)
-VALUES (300, 'IT');
